8115e84cf1191bdcb47abff90ca008a8ad0bbc75
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2021-09-20 12:33:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent brand="Apple" price="50000"></MyComponent>
+      <MyComponent brand="Microsoft" price="5000"></MyComponent>
+      <MyComponent brand="Google" price="0"></MyComponent>
+      <MyComponent></MyComponent>
     </div>
   );
 }
 
+function MyComponent(props) {
+  const [points, setPoints] = useState(1)
+  const MyStyle = {
+    borderRadius: '10px',
+    border: '3px solid royalblue'
+  };
+
+  const handleAddPoint = () => {
+    const newPoints = points * 2;
+    setPoints(newPoints);
+  }
+  return (
+    <div className="myone" style={{ padding: '10px'/* , margin: '20px' */ }} style={MyStyle}>
+      <h1>Yo yo mama! {props.brand}</h1>
+      <button onClick={handleAddPoint}>Add Point</button>
+      <p>asking rate: {props.price}, Mypoints: {points}</p>
+      <p>I can write and edit my component</p>
+    </div>
+  )
+}
 export default App;
